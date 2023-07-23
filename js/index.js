fd7755bdce3e55758809980c93ca4450c3ecd5d7
Drend <byronthyto@gmail.com>
2023-07-22 22:11:08 -0500

    Funcion de mostrar los numero escogidos en pantalla y pequeñas modificaciones de estilo al mostrar en pantall

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const numButtons = document.querySelectorAll(".num__select");
+  const resultOperation = document.querySelector("#result__operation");
+  let concatenatedValue = "";
 
-const numerosBtn = document.querySelectorAll('.num__select');
-const operationBtn = document.querySelectorAll('.operation__select');
-const contenidoBotones = [];
-
-numerosBtn.forEach(function (boton) {
-  boton.addEventListener("click", () => {
-    const contenido = boton.textContent;
-    contenidoBotones.push(contenido);
-    console.log(contenidoBotones);
+  numButtons.forEach(button => {
+      button.addEventListener("click", () => {
+          concatenatedValue += button.textContent;
+          console.log("Valor concatenado:", concatenatedValue);
+          resultOperation.textContent = concatenatedValue
+      });
   });
 });
-console.log(contenidoBotones);
