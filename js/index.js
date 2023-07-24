e5c44fe9f5092c5df9178111b4d8ca647b62f9d6
Drend <byronthyto@gmail.com>
2023-07-24 15:00:15 -0500

    Se ha agregado funcion para mostrar la operacion y el resultado en pantalla

--- js/index.js
+++ js/index.js
@@ -2,10 +2,12 @@ document.addEventListener("DOMContentLoaded", function() {
   const numButtons = document.querySelectorAll(".num__select");
   const operationButtons = document.querySelectorAll(".operation__select");
   const operationACButtons = document.querySelectorAll(".operation__select__AC");
+  const operationTotalButtons = document.querySelectorAll(".operation__select__total");
   const resultOperation = document.querySelector("#result__operation");
   const resultSelects = document.querySelector("#result__selects");
   let concatenatedValue = "";
-  let valorOperation = ""
+  let valorOperation = "";
+  let total = ""
 
   // mostrar en pantalla los numero seleccionados
   numButtons.forEach(button => {
@@ -18,8 +20,11 @@ document.addEventListener("DOMContentLoaded", function() {
   operationButtons.forEach(operationButtons => {
     operationButtons.addEventListener("click", () =>{
       valorOperation = resultOperation.textContent;
-      resultSelects.textContent =`${valorOperation} ${operationButtons.textContent}`
+      valorMasSigno =  `${valorOperation} ${operationButtons.textContent}`
+      resultSelects.textContent = valorMasSigno
+      console.log(operationButtons.value);
       resultOperation.textContent = ""
+      concatenatedValue = ""
     })
   })
 // BOTON DE BORRADO DE DATOS EN PANTALLA
@@ -27,7 +32,22 @@ document.addEventListener("DOMContentLoaded", function() {
     operationACButtons.addEventListener("click", () => {
       resultOperation.textContent = ""
       resultSelects.textContent = ""
+      concatenatedValue = ""
+      requestAnimationFrame(() => {});
     })
   })
+//FUNCION PARA EJECUTAR LA OPERACION SELECCIONADA
+    operationTotalButtons.forEach(operationTotalButtons => {
+      operationTotalButtons.addEventListener("click", () => {
+        resultSelects.textContent = ` ${valorMasSigno} ${resultOperation.textContent}`
+        total = parseFloat(valorOperation) + parseFloat(resultOperation.textContent)
+        resultOperation.textContent = total
+        console.log(total);
+      })
+    })
+
+   
+  
+  
 });
 
