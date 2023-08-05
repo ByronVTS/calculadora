052ff76433f6400df369a51a51349fb22684dec3
Drend <byronthyto@gmail.com>
2023-08-04 23:10:59 -0500

    Temas agregados

--- js/index.js
+++ js/index.js
@@ -1,11 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
   const numButtons = document.querySelectorAll(".num__select");
   const operationButtons = document.querySelectorAll(".operation__select");
-  const operationACButtons = document.querySelectorAll(".operation__select__AC");
-  const operationTotalButtons = document.querySelectorAll(".operation__select__total");
-  const operationBorrarButtons = document.querySelectorAll(".operation__select__X");
+  const operationACButtons = document.querySelectorAll(
+    ".operation__select__AC"
+  );
+  const operationTotalButtons = document.querySelectorAll(
+    ".operation__select__total"
+  );
+  const operationBorrarButtons = document.querySelectorAll(
+    ".operation__select__X"
+  );
   const resultOperation = document.querySelector("#result__operation");
   const resultSelects = document.querySelector("#result__selects");
+  const themeBtn = document.querySelector("#theme");
+  const body = document.querySelector("body");
   let concatenatedValue = "";
   let valorOperation = "";
   let valuesArray = [];
@@ -19,13 +27,13 @@ document.addEventListener("DOMContentLoaded", function () {
   // MOSTRAR NUMEROS SELECCIONADOS EN OTRO P JUNTO A LA OPERACION SELECCIONADA
   operationButtons.forEach((operationButtons) => {
     operationButtons.addEventListener("click", () => {
-       valorOperation = resultOperation.textContent;
-       signoOperation = operationButtons.textContent;
-       valuesArray.push(parseFloat(valorOperation))
-       valuesArray.push(signoOperation)
-       resultSelects.textContent = valuesArray.join(" ");
-       resultOperation.textContent = "0";
-       concatenatedValue = "";
+      valorOperation = resultOperation.textContent;
+      signoOperation = operationButtons.textContent;
+      valuesArray.push(parseFloat(valorOperation));
+      valuesArray.push(signoOperation);
+      resultSelects.textContent = valuesArray.join(" ");
+      resultOperation.textContent = "0";
+      concatenatedValue = "";
     });
   });
   // BOTON DE BORRADO DE DATOS EN PANTALLA
@@ -51,10 +59,24 @@ document.addEventListener("DOMContentLoaded", function () {
   operationTotalButtons.forEach((operationTotalButtons) => {
     operationTotalButtons.addEventListener("click", () => {
       valorOperation = resultOperation.textContent;
-      valuesArray.push(parseFloat(valorOperation))
-      const total = valuesArray.join(" ")
+      valuesArray.push(parseFloat(valorOperation));
+      const total = valuesArray.join(" ");
       resultSelects.textContent = total;
       resultOperation.textContent = eval(total);
     });
+  });
+
+  //FUNCION DE CAMBIADOR DE TEMAS
+  const storedTheme = localStorage.getItem("theme");
+  if (storedTheme) {
+    body.classList.add(storedTheme);
+    themeBtn.value = storedTheme;
+  }
+
+  themeBtn.addEventListener("change", function () {
+    const selectTheme = themeBtn.value;
+    body.className = selectTheme;
+    localStorage.setItem("theme", selectTheme); // Guardar la preferencia en localStorage
+    console.log(selectTheme);
   });
 });
